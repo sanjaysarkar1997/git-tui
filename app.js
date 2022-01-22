c95310488655d902ce3142fd22de77e4a34920d6
Sanjay Sarkar <sanjaysarkar430@gmail.com>
2022-01-22 14:38:15 +0530

    update

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ function todo(toDos) {
 
 todo(toDos);
 let data = [
-  { name: "John", age: 30, car: null },
   { name: "Mike", age: 26, car: "BMW" },
   { name: "Jane", age: 28, car: "Toyota" },
   { name: "Mary", age: 32, car: "Mercedes" },
