cdcbeadb2f0f98088aa808d529e7a15026001a5c
Ankita Mohanta <ankitamohanta16@gmail.com>
2022-01-22 14:17:38 +0530

    add

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-// const a = {
-//   b: "ankita",
-//   c: 34,
-//   d: [2, 3, 4],
-// };
-// const tasks = [
-//   {
-//     b: 23,
-//   },
-//   {
-//     b: 23,
-//   },
-// ];
-// const todos=["ankita","arpita","priya"]
-// function todo(todos){
-//     for(var x=0;x<todos.length;x++)
-//      console.log(todos[x])
-// }
-// todo(todos)
-const todoDiv=document.getElementById("todo-ul")
-console.log(todoDiv)
-todoDiv.innerHTML="<li>about</li>"
-// console.log(todos)
-// console.log(a);
+const a = {
+  b: "ankita",
+  c: 34,
+  d: [2, 3, 4],
+};
+const tasks = [
+  {
+    b: 23,
+  },
+  {
+    b: 23,
+  },
+];
+const todos = ["ankita", "arpita", "priya"];
+function todo(todos) {
+  let to = " ";
+  for (var x = 0; x < todos.length; x++) {
+      to=todos
+  }
+}
+todo(todos);
+const todoDiv = document.getElementById("todo-ul");
+let todoList = "<li>ankita</li> <li>arpita</li><li>priya</li>";
+todoDiv.innerHTML = todoList;
+
+console.log(todoDiv);
+console.log(todos);
+console.log(a);
