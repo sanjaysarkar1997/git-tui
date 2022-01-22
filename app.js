8bf78ed6b2290df8b73d48a90690925ef26164ed
Ankita Mohanta <ankitamohanta16@gmail.com>
2022-01-22 14:41:42 +0530

    add changes

--- app.js
+++ app.js
@@ -10,15 +10,5 @@ function todo(toDos) {
 }
 
 todo(toDos);
-let data = [
-  { name: "John", age: 30, car: null },
-  { name: "Mike", age: 26, car: "BMW" },
-  { name: "Jane", age: 28, car: "Toyota" },
-  { name: "Mary", age: 32, car: "Mercedes" },
-  { name: "Mark", age: 25, car: "BMW" },
-  { name: "Pam", age: 27, car: "Toyota" },
-  { name: "Sam", age: 29, car: "Mercedes" },
-  { name: "Phil", age: 31, car: "BMW" },
-  { name: "Liz", age: 33, car: "Toyota" },
-  { name: "Joe", age: 35, car: "Mercedes" },
-];
+let data = { name: "John", age: 30, car: null };
+
